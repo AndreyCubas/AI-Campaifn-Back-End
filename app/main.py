d9c69baa6eada4c14c8797d097bc6bb373f8e851
AndreyCubas <Felipedreycubascardoso@gmail.com>
2026-01-28 15:43:59 -0300

    fix: Atualizando conecxao do main

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ Base.metadata.create_all(bind=engine)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5173", "http://127.0.0.1:3000"],
+    allow_origins=["http://localhost:8000", "http://localhost:8080", "http://127.0.0.1:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
